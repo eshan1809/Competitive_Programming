ff03b4f9e81dddbaf2eb5ea8ec757e49d619e488
Eshan Bhatt <eshan18.09@gmail.com>
2021-09-27 21:22:06 +0530

    segment tree

--- Segment Tree/MaxInAnInterval_RangeQueryPointUpdate.java
+++ Segment Tree/MaxInAnInterval_RangeQueryPointUpdate.java
@@ -17,6 +17,101 @@ Implement the SegmentTree class:
 Can u do it in O(log(n)) or better Time Complexity.
 */
 
+import java.io.*;
+
 public class MaxInAnInterval_RangeQueryPointUpdate {
+
+    public static class SegmentTree {
+        int[] arr, tree;
+
+        SegmentTree(int arr[]) {
+            this.arr = arr;
+            tree = new int[4 * arr.length];
+            build(1, 0, arr.length - 1);
+        }
+
+        void build(int id, int start, int end) {
+            if (start == end)
+                tree[id] = arr[start];
+            else {
+                int mid = (start + end) / 2, left = 2 * id, right = 2 * id + 1;
+                build(left, start, mid);
+                build(right, mid + 1, end);
+                tree[id] = Math.max(tree[left], tree[right]);
+            }
+        }
+
+        void update(int pos, int val) {
+            update(1, 0, arr.length - 1, pos, val);
+        }
+
+        void update(int id, int start, int end, int pos, int val) {
+            if (start == end) {
+                arr[pos] = val;
+                tree[id] = val;
+            } else {
+                int mid = (start + end) / 2, left = 2 * id, right = 2 * id + 1;
+                if (pos <= mid)
+                    update(left, start, mid, pos, val);
+                else
+                    update(right, mid + 1, end, pos, val);
+                tree[id] = Math.max(tree[left], tree[right]);
+            }
+        }
+
+        int query(int l, int r) {
+            return find(1, 0, arr.length - 1, l, r);
+        }
+
+        int find(int id, int start, int end, int l, int r) {
+            if (start > r || end < l)
+                return Integer.MIN_VALUE;
+            else if (start == end)
+                return tree[id];
+            else if (l <= start && end <= r)
+                return tree[id];
+            else {
+                int mid = (start + end) / 2;
+                int left = find(2 * id, start, mid, l, r);
+                int right = find(2 * id + 1, mid + 1, end, l, r);
+                return Math.max(left, right);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(read.readLine());
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(read.readLine());
+        }
+
+        SegmentTree obj = new SegmentTree(arr);
+
+        int q = Integer.parseInt(read.readLine());
+
+        StringBuilder out = new StringBuilder();
+        while (q-- > 0) {
+            String[] inp = read.readLine().split(" ");
+
+            int t = Integer.parseInt(inp[0]);
+            int l = Integer.parseInt(inp[1]);
+            int r = Integer.parseInt(inp[2]);
+
+            if (t == 0) {
+                obj.update(l, r);
+            } else {
+                int ans = obj.query(l, r);
+                out.append(ans);
+                out.append("\n");
+            }
+        }
+
+        System.out.println(out);
+    }
 
 }
