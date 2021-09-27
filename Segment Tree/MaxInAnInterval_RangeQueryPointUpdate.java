928dc1513d58ae978247405a1d591c7557270c67
Eshan Bhatt <eshan18.09@gmail.com>
2021-09-27 20:20:44 +0530

    segment tree

---
+++ Segment Tree/MaxInAnInterval_RangeQueryPointUpdate.java
@@ -0,0 +1,22 @@
+/*
+You are given an array(of integers) of length n.
+You are required to answer q queries.
+
+Queries can be of two types
+0. 0 pos val : In this you have to update arr[pos] to val.
+1. 1 l r: In this query u have to find the max among all elements in this interval.
+
+
+To do the above task u have to create a datastructure as follows :-
+
+Implement the SegmentTree class:
+1. SegmentTree(int arr[]): Initializes the SegmentTree object with an array,
+2. void update(int pos, int val): updates the arr[pos] to val,
+3. int query(int l, int r): return max of all element's in interval [l, r].
+
+Can u do it in O(log(n)) or better Time Complexity.
+*/
+
+public class MaxInAnInterval_RangeQueryPointUpdate {
+
+}
