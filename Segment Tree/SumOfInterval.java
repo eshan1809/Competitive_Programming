ac7dc954529c0db63c2b6b6732bd7032b79b2d02
Eshan Bhatt <eshan18.09@gmail.com>
2021-09-27 22:31:36 +0530

    segment tree

---
+++ Segment Tree/SumOfInterval.java
@@ -0,0 +1,21 @@
+/*
+You are given an array(of integers) of length n.
+You are required to answer q queries.
+
+Queries can be of two types
+0. 0 pos val : In this you have to update arr[pos] to val.
+1. 1 l r: In this query u have to find the sum of all elements in this interval.
+
+Sum of elements in interval [l, r] means sum of all arr[i] for which i is in range [l, r].
+
+To do the above task u have to create a datastructure as follows :-
+
+Implement the SegmentTree class:
+1. SegmentTree(int arr[]): Initializes the SegmentTree object with an array,
+2. void update(int pos, int val): updates the arr[pos] to val,
+3. int query(int l, int r): return sum of all element's in interval [l, r].
+*/
+
+public class SumOfInterval {
+
+}
