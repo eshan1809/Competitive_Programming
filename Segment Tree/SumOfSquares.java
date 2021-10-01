7d5593a3929a6494b2e4ffc2441cb7871ef5546b
Eshan Bhatt <eshan18.09@gmail.com>
2021-10-01 12:03:56 +0530

    added more problems

--- Segment Tree/SumOfSquares.java
+++ Segment Tree/SumOfSquares.java
@@ -19,17 +19,84 @@ import java.io.*;
 public class SumOfSquares {
 
     public static class SegmentTree {
+        int n;
+        int[] arr, sum, tree;
+        long[] squareSum;
 
         SegmentTree(int arr[]) {
+            n = arr.length;
+            this.arr = arr;
+            sum = new int[4 * n];
+            tree = new int[4 * n];
+            squareSum = new long[4 * n];
+            build(1, 0, n - 1);
+        }
 
+        void build(int id, int start, int end) {
+            if (start == end) {
+                sum[id] = arr[start];
+                squareSum[id] = (long) arr[start] * arr[start];
+            } else {
+                int mid = (start + end) / 2, left = 2 * id, right = left + 1;
+                build(left, start, mid);
+                build(right, mid + 1, end);
+                sum[id] = sum[left] + sum[right];
+                squareSum[id] = squareSum[left] + squareSum[right];
+            }
         }
 
         void update(int l, int r, int val) {
+            update(1, 0, n - 1, l, r, val);
+        }
 
+        void update(int id, int start, int end, int l, int r, int val) {
+            distribute(id, start, end);
+            if (end < l || r < start)
+                return;
+            else if (start == end) {
+                tree[id] += val;
+                distribute(id, start, end);
+            } else if (l <= start && end <= r) {
+                tree[id] += val;
+                distribute(id, start, end);
+            } else {
+                int mid = (start + end) / 2, left = 2 * id, right = left + 1;
+                update(left, start, mid, l, r, val);
+                update(right, mid + 1, end, l, r, val);
+                sum[id] = sum[left] + sum[right];
+                squareSum[id] = squareSum[left] + squareSum[right];
+            }
         }
 
         long query(int l, int r) {
-            return 0L;
+            return query(1, 0, n - 1, l, r);
+        }
+
+        long query(int id, int start, int end, int l, int r) {
+            if (end < l || r < start)
+                return 0L;
+            distribute(id, start, end);
+            if (start == end) {
+                return squareSum[id];
+            } else if (l <= start && end <= r) {
+                return squareSum[id];
+            } else {
+                int mid = (start + end) / 2, left = 2 * id, right = left + 1;
+                return query(left, start, mid, l, r) + query(right, mid + 1, end, l, r);
+            }
+        }
+
+        void distribute(int id, int start, int end) {
+            if (start == end) {
+                sum[id] += tree[id];
+                squareSum[id] = 1L * sum[id] * sum[id];
+            } else {
+                squareSum[id] += (long) (end - start + 1) * tree[id] * tree[id] + 2L * tree[id] * sum[id];
+                sum[id] += (end - start + 1) * tree[id];
+                tree[2 * id] += tree[id];
+                tree[2 * id + 1] += tree[id];
+            }
+            tree[id] = 0;
         }
 
     }
