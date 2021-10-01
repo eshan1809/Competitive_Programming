ad8b625176201058c11f84817b7e9eaf9889d941
Eshan Bhatt <eshan18.09@gmail.com>
2021-10-01 14:48:22 +0530

    added more problems

--- Segment Tree/ToggleBulbs.java
+++ Segment Tree/ToggleBulbs.java
@@ -6,6 +6,18 @@ You have to perform 2 types of operations:-
 2. Count how many bulbs numbered between A to B are on.This is represented by 2 A B.
 */
 
-public class ToggleBulbs {
+import java.io.*;
+import java.util.*;
 
+public class ToggleBulbs {
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String inps[] = read.readLine().split(" ");
+        int n = Integer.parseInt(inps[0]);
+        int q = Integer.parseInt(inps[1]);
+
+        // write your code here
+
+    }
 }
