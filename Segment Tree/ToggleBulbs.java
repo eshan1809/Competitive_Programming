b11a7585e6eb33965e18387d0cda27ce867c3234
Eshan Bhatt <eshan18.09@gmail.com>
2021-10-01 12:05:32 +0530

    added more problems

---
+++ Segment Tree/ToggleBulbs.java
@@ -0,0 +1,11 @@
+/*
+You are given n bulbs, numbered from 1 to n. Initially all the bulbs are turned off.
+You have to perform 2 types of operations:-
+
+1. Toggle all bulds numbered between A to B.Here toggle mean turn on bulbs to off and off bulbs to on.This is represented by 1 A B.
+2. Count how many bulbs numbered between A to B are on.This is represented by 2 A B.
+*/
+
+public class ToggleBulbs {
+
+}
