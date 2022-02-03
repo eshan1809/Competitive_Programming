87407e8d86bfb98bcf666d83ad1213f95f1205b0
Eshan Bhatt <eshan18.09@gmail.com>
2022-02-03 17:03:18 +0530

    Number Theory added

---
+++ Number Theory/ExtendedEuclideanAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExtendedEuclideanAlgorithm {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt(), b = scn.nextInt();
+        int[] ans = solve(a, b);
+        System.out.println(ans[0] + " " + ans[1]);
+        scn.close();
+    }
+
+    private static int[] solve(int a, int b) {
+        if (b == 0)
+            return new int[] { 1, 0 };
+        int[] ans = solve(b, a % b);
+        return new int[] { ans[1], ans[0] - (a / b) * ans[1] };
+    }
+}
