aaf4360f64fd2c7b2a5ef429582ad023979ac1fd
Eshan Bhatt <eshan18.09@gmail.com>
2022-02-03 17:27:57 +0530

    more problems added

---
+++ Number Theory/LinearDiophantineEquation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LinearDiophantineEquation {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt(), b = scn.nextInt(), k = scn.nextInt();
+        int[] ans = solve(a, b);
+        System.out.println(1L * k * ans[0] + " " + 1L * k * ans[1]);
+        scn.close();
+    }
+
+    private static int[] solve(int a, int b) {
+        if (b == 0)
+            return new int[] { 1, 0 };
+        int[] ans = solve(b, a % b);
+        return new int[] { ans[1], ans[0] - (a / b) * ans[1] };
+    }
+}
