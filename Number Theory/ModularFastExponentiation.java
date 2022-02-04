afd67490ea8361b22d85b2d61ecbf967d0278cc0
Eshan Bhatt <eshan18.09@gmail.com>
2022-02-04 19:05:07 +0530

    more problems added

---
+++ Number Theory/ModularFastExponentiation.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ModularFastExponentiation {
+    public static int xpown(long x, long n, long p) {
+        if (n == 0)
+            return 1;
+        int ans = xpown(x, n >> 1, p);
+        ans %= p;
+        ans = (int) ((1L * ans * ans) % p);
+        if ((n & 1) == 1)
+            ans = (int) ((x * ans) % p);
+        return ans;
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner scn = new Scanner(System.in);
+
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+
+        int ans = xpown(x, n, 1000000007);
+        System.out.println(ans);
+        scn.close();
+    }
+}
